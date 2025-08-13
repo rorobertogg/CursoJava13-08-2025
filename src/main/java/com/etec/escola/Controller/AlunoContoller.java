01e864d77c90b9bc48a4aad604dbf6b3f0d0acfd
rorobertogg <joaorobero22@outlook.com>
2025-08-13 17:21:27 -0300

    feat;

--- src/main/java/com/etec/escola/Controller/AlunoContoller.java
+++ src/main/java/com/etec/escola/Controller/AlunoContoller.java
@@ -1,4 +1,12 @@
 package com.etec.escola.Controller;
 
+import com.etec.escola.Services.AlunoService;
+
 public class AlunoContoller {
+
+    private final AlunoService alunoService;
+
+    public AlunoContoller(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
 }
